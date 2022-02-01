a7132bc3ddd7192390dee69abde5dbb32fc1e632
Brandon Gibbs <brandon@webonyx.com>
2022-02-01 17:51:56 -0600

    Undo chromatic setup thing

--- src/stories/UserLink.js
+++ src/stories/UserLink.js
@@ -6,7 +6,7 @@ export const UserLink = forwardRef(({ children, user, to = '/people/:id', ...res
   const props = { to: { pathname: to.replace(':id', encodeURIComponent(user.attuid) || user.id), state: user } }
   return (
     <RouterLink ref={ref} {...props} {...rest}>
-      <div style={{fontSize:"30px"}}>{children || user.name}</div>
+      <div>{children || user.name}</div>
     </RouterLink>
   )
 })
