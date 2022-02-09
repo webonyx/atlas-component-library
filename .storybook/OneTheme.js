946d959c07f1a655775f0b3bec437840ae39cd85
jared <jared@webonyx.com>
2022-02-08 16:58:27 -0800

    Bootstrap boilerplate

---
+++ .storybook/OneTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+import brandImage from './logo.svg'
+
+export default create({
+  base: 'light',
+  brandTitle: 'CDO UI Storybook',
+  brandUrl: 'https://data.att.com',
+  brandImage,
+})
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons'
+import theme from './OneTheme'
+
+addons.setConfig({
+  theme,
+})
