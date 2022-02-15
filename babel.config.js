20388288474a09660782ff599f49447c8d0cd844
Viet Pham <viet@webonyx.com>
2022-02-15 15:14:01 +0700

    Add build scripts

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/transform-runtime',
+      'babel-plugin-emotion',
+    ],
+  };
+};
