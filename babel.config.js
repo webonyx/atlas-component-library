81c7afd0eebb26543c14c4b9d1a95461f291a130
jared <jared@webonyx.com>
2022-05-26 13:01:10 -0700

    Fix build command

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
   api.cache(true)
 
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/transform-runtime',
